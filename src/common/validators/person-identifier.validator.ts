7b6896c1f983709ec6b95fc68c7e0499ba7193bf
Gabriel Silva <devzgabriel@gmail.com>
2022-03-23 09:42:45 -0300

    feat: validators and transformers from seed

---
+++ src/common/validators/person-identifier.validator.ts
@@ -0,0 +1,38 @@
+import { validateCNPJ, validateCPF } from '@euk-labs/beltz';
+import { buildMessage, ValidateBy, ValidationOptions } from 'class-validator';
+
+export function validateCNPJOrCPF(value: string) {
+  if (!value) {
+    return true;
+  }
+
+  const onlyDigits = value.replace(/\D/g, '');
+
+  if (onlyDigits.length === 11) {
+    return validateCPF(onlyDigits);
+  }
+
+  if (onlyDigits.length === 14) {
+    return validateCNPJ(onlyDigits);
+  }
+
+  return false;
+}
+
+export function IsValidCNPJOrCPF(
+  validationOptions?: ValidationOptions,
+): PropertyDecorator {
+  return ValidateBy(
+    {
+      name: 'isValidCep',
+      validator: {
+        validate: (value): boolean => validateCNPJOrCPF(value),
+        defaultMessage: buildMessage(
+          (eachPrefix) => eachPrefix + '$property must be a valid CNPJ or CPF',
+          validationOptions,
+        ),
+      },
+    },
+    validationOptions,
+  );
+}
